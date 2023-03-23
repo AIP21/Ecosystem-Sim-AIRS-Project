8718a7f92f15d9b912429fd29f3cb69d0150fa2b
Alexander IP <44927160+AIP21@users.noreply.github.com>
2023-03-23 14:46:42 -0400

    Added tests to show that the data structure works
    Added a few tests to show and visualize that the data structure is functional.
    Added a data type for a tree.
    
    Now I need to move the code I wrote for a tree growth test into the main project and add it to the data structure.

--- Assets/DataStructure/Scripts/DataStructure.cs
+++ Assets/DataStructure/Scripts/DataStructure.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace DataStructure
+namespace SimDataStructure
 {
     public class DataStructure : MonoBehaviour
     {
@@ -14,17 +14,81 @@ namespace DataStructure
         public List<Grid> Grids = new List<Grid>();
 
         [Header("Debug")]
+        public bool DebugDraw = false;
         [Range(0f, 10f)]
         public float LevelZShift = 3f;
+        public bool DebugTests = false;
+        public bool TestSingleLevel = false;
+        public int TestLevel = 0;
+        public Vector2 TestPosition = new Vector2(0, 0);
+        public List<GridCell> TestedCells = new List<GridCell>();
+        public GenericDictionary<string, string> TestedCellData = new GenericDictionary<string, string>();
+        public int TestedCellCount = 0;
 
-        public void Start(){
+        private Vector2 lastTestPos = new Vector2(0, 0);
+
+        public void Awake()
+        {
             // Create and populate the grids
-            populateGrids(Levels);
+            this.populateGrids(Levels);
+            this.performDebugTests(true);
         }
 
         public void FixedUpdate()
         {
-            
+            if (DebugTests)
+            {
+                this.performDebugTests();
+            }
+        }
+
+        private void performDebugTests(bool force = false)
+        {
+            if (!force && lastTestPos == TestPosition)
+                return;
+
+            Debug.Log("Test Position: " + TestPosition);
+
+            lastTestPos = TestPosition;
+
+            if (TestSingleLevel)
+            {
+                Grid grid = Grids[TestLevel];
+                GridCell cell = grid.GetCell(TestPosition);
+                TestedCells.Clear();
+
+                if (cell != null)
+                {
+                    TestedCells.Add(cell);
+
+                    Dictionary<string, AbstractGridData> data = cell.GetData();
+                    TestedCellData.Clear();
+
+                    if (data == null)
+                    {
+                        TestedCellData.Add("NULL", "Data is null");
+                    }
+                    else if (data.Count == 0){
+                        TestedCellData.Add("NULL", "No data");
+                    }
+                    else
+                    {
+                        foreach (KeyValuePair<string, AbstractGridData> d in data)
+                        {
+                            TestedCellData.Add(d.Key, d.Value.ToString());
+                        }
+                    }
+                }
+
+                TestedCellCount = TestedCells.Count;
+            }
+            else if (Grids.Count > 0)
+            {
+                // Query the highest level grid for all cells that contian the test position
+                // It should return a list of grid cells that contain the test position
+                TestedCells = Grids[Grids.Count - 1].GetCells(TestPosition);
+                TestedCellCount = TestedCells.Count;
+            }
         }
 
         /*
@@ -61,13 +125,14 @@ namespace DataStructure
                 foreach (GridCell cell in grid.Cells)
                 {
                     // Get the cell in the parent that contains the current cell's center position
-                    GridCell containerCell = gridAbove.GetCellAt(cell.center);
+                    GridCell containerCell = gridAbove.GetCell(cell.center);
 
-                    if (containerCell == null) {
+                    if (containerCell == null)
+                    {
                         print("Container cell is null");
                         continue;
                     }
-                    
+
                     // Assign the parent cell to the container cell
                     cell.parentCell = containerCell;
                     containerCell.childCells.Add(cell);
@@ -77,11 +142,31 @@ namespace DataStructure
 
         private void OnDrawGizmos()
         {
-            if (Grids != null && Grids.Count > 0){
-                foreach (Grid grid in Grids)
-                {                    
-                    if (grid != null)
-                        grid.DebugDrawGrid(LevelZShift);
+            if (DebugDraw)
+            {
+                if (Grids != null && Grids.Count > 0)
+                {
+                    foreach (Grid grid in Grids)
+                    {
+                        if (grid != null)
+                            grid.DebugDrawGrid(LevelZShift);
+                    }
+                }
+
+                // Draw a green box around the tested cells
+                if (TestedCells != null)
+                {
+                    foreach (GridCell cell in TestedCells)
+                    {
+                        Gizmos.color = Color.green;
+
+                        Vector3 center = new Vector3(cell.center.x, (cell.level.Level - 1) * LevelZShift, cell.center.y);
+
+                        Gizmos.DrawLine(center + new Vector3(-cell.level.CellSize.x / 2, 0, -cell.level.CellSize.y / 2), center + new Vector3(cell.level.CellSize.x / 2, 0, -cell.level.CellSize.y / 2));
+                        Gizmos.DrawLine(center + new Vector3(cell.level.CellSize.x / 2, 0, -cell.level.CellSize.y / 2), center + new Vector3(cell.level.CellSize.x / 2, 0, cell.level.CellSize.y / 2));
+                        Gizmos.DrawLine(center + new Vector3(cell.level.CellSize.x / 2, 0, cell.level.CellSize.y / 2), center + new Vector3(-cell.level.CellSize.x / 2, 0, cell.level.CellSize.y / 2));
+                        Gizmos.DrawLine(center + new Vector3(-cell.level.CellSize.x / 2, 0, cell.level.CellSize.y / 2), center + new Vector3(-cell.level.CellSize.x / 2, 0, -cell.level.CellSize.y / 2));
+                    }
                 }
             }
         }
@@ -111,37 +196,42 @@ namespace DataStructure
 
             this.Cells = new List<GridCell>();
             this.ChildCells = new List<GridCell>();
-            
+
             this.XCellCount = (int)(size.x / gridLevel.CellSize.x);
             this.YCellCount = (int)(size.y / gridLevel.CellSize.y);
 
-            createCells();
+            populate();
         }
 
-        private void createCells() {
+        private void populate()
+        {
             for (int y = 0; y < YCellCount; y++)
             {
                 for (int x = 0; x < XCellCount; x++)
                 {
                     // Calculate the center position using size
                     Vector2 pos = new Vector2(x * gridLevel.CellSize.x + gridLevel.CellSize.x / 2, y * gridLevel.CellSize.y + gridLevel.CellSize.y / 2);
-                    
+
                     GridCell cell = new GridCell(this.gridLevel, pos);
                     Cells.Add(cell);
 
                     // Assign this cell's neighbors to the cell above and to the left
-                    if (y > 0){
+                    if (y > 0)
+                    {
                         cell.neighbors[0] = Cells[Cells.Count - XCellCount - 1];
                     }
-                    if (x > 0) {
+                    if (x > 0)
+                    {
                         cell.neighbors[1] = Cells[Cells.Count - 2];
                     }
-                    
+
                     // Assign the cell above and to the left's neighbors to this cell
-                    if (y > 0){
+                    if (y > 0)
+                    {
                         Cells[Cells.Count - XCellCount - 1].neighbors[2] = cell;
                     }
-                    if (x > 0){
+                    if (x > 0)
+                    {
                         Cells[Cells.Count - 2].neighbors[3] = cell;
                     }
                 }
@@ -151,23 +241,42 @@ namespace DataStructure
         /**
         Return the cell that contains the query point
         **/
-        public GridCell GetCellAt(Vector2 queryPoint) {
+        public GridCell GetCell(Vector2 queryPoint)
+        {
             // Calculate the index of the cell that contains the query point
             int index = (int)(queryPoint.x / gridLevel.CellSize.x) + (int)(queryPoint.y / gridLevel.CellSize.y) * XCellCount;
-            
+
             if (index < 0 || index >= Cells.Count)
                 return null;
             else
                 return Cells[index];
         }
 
-        public void DebugDrawGrid(float levelZShift) {
-            foreach (GridCell cell in Cells) {
+        /**
+        Return the path of cells to the lowest-level cell that contains the query point
+        **/
+        public List<GridCell> GetCells(Vector2 queryPoint)
+        {
+            GridCell container = GetCell(queryPoint);
+            if (container != null)
+            {
+                List<GridCell> path = container.GetLowestChild(queryPoint);
+                path.Add(container);
+                return path;
+            }
+
+            return null;
+        }
+
+        public void DebugDrawGrid(float levelZShift)
+        {
+            foreach (GridCell cell in Cells)
+            {
                 Gizmos.color = color;
-                
+
                 // Draw gizmo square using lines centered around cell center
                 Vector3 center = new Vector3(cell.center.x, (this.gridLevel.Level - 1) * levelZShift, cell.center.y);
-                
+
                 Gizmos.DrawLine(center + new Vector3(-gridLevel.CellSize.x / 2, 0, -gridLevel.CellSize.y / 2), center + new Vector3(gridLevel.CellSize.x / 2, 0, -gridLevel.CellSize.y / 2));
                 Gizmos.DrawLine(center + new Vector3(gridLevel.CellSize.x / 2, 0, -gridLevel.CellSize.y / 2), center + new Vector3(gridLevel.CellSize.x / 2, 0, gridLevel.CellSize.y / 2));
                 Gizmos.DrawLine(center + new Vector3(gridLevel.CellSize.x / 2, 0, gridLevel.CellSize.y / 2), center + new Vector3(-gridLevel.CellSize.x / 2, 0, gridLevel.CellSize.y / 2));
@@ -175,18 +284,34 @@ namespace DataStructure
 
                 // For each neighbor, draw a small line to the edge of this cell
                 Gizmos.color = Color.white;
-                for (int i = 0; i < cell.neighbors.Length; i++) {
-                    if (cell.neighbors[i] != null) {
-                        if (i == 0) { // above
+                for (int i = 0; i < cell.neighbors.Length; i++)
+                {
+                    if (cell.neighbors[i] != null)
+                    {
+                        if (i == 0)
+                        { // above
                             Gizmos.DrawLine(center, new Vector3(center.x, (this.gridLevel.Level - 1) * levelZShift, center.z - gridLevel.CellSize.y / 3));
-                        } else if (i == 1) { // left
+                        }
+                        else if (i == 1)
+                        { // left
                             Gizmos.DrawLine(center, new Vector3(center.x - gridLevel.CellSize.x / 3, (this.gridLevel.Level - 1) * levelZShift, center.z));
-                        } else if (i == 2) { // below
+                        }
+                        else if (i == 2)
+                        { // below
                             Gizmos.DrawLine(center, new Vector3(center.x, (this.gridLevel.Level - 1) * levelZShift, center.z + gridLevel.CellSize.y / 3));
-                        } else if (i == 3) { // right
+                        }
+                        else if (i == 3)
+                        { // right
                             Gizmos.DrawLine(center, new Vector3(center.x + gridLevel.CellSize.x / 3, (this.gridLevel.Level - 1) * levelZShift, center.z));
                         }
                     }
+                }
+
+                // Draw a line to the parent cell
+                if (cell.parentCell != null)
+                {
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawLine(center, new Vector3(cell.parentCell.center.x, (cell.parentCell.level.Level - 1) * levelZShift, cell.parentCell.center.y));
                 }
             }
         }
@@ -194,47 +319,100 @@ namespace DataStructure
 
     public class GridCell
     {
-        public int level;
         public Vector2 center;
+        public Bounds bounds;
+        public GridLevel level;
 
         public GridCell parentCell;
         public List<GridCell> childCells = new List<GridCell>();
         public GridCell[] neighbors = new GridCell[4]; // 0 = above, 1 = left, 2 = below, 3 = right
 
-        public Dictionary<string, AbstractGridData> data = new Dictionary<string, AbstractGridData>();
-        
-        public GridCell(GridLevel level, Vector2 center) {
-            this.center = center;
-            this.level = level.Level;
+        public GenericDictionary<string, AbstractGridData> data = new GenericDictionary<string, AbstractGridData>();
 
-            this.setupLevelData(level);
+        public GridCell(GridLevel level, Vector2 center)
+        {
+            this.center = center;
+            this.level = level;
+            this.bounds = new Bounds(center, level.CellSize);
+
+            this.setupLevelData();
         }
 
-        private void setupLevelData(GridLevel level){
-            foreach(KeyValuePair<string, DataType> dataEntry in level.data) {
-                switch(dataEntry.Value) {
-                    case Float:
-                        this.dataFloat.Add(dataEntry.Key, new GridDaa<Float>(0.0));
+        private void setupLevelData()
+        {
+            foreach (KeyValuePair<string, DataType> dataEntry in level.data)
+            {
+                switch (dataEntry.Value)
+                {
+                    case DataType.Float:
+                        this.AddData(dataEntry.Key, new GridData<float>(4.20f));
                         break;
-                    case Int:
-                        this.dataInt.Add(dataEntry.Key, 0);
+                    case DataType.Int:
+                        this.AddData(dataEntry.Key, new GridData<int>(69));
                         break;
-                    case Bool:
-                        this.dataBool.Add(dataEntry.Key, false);
+                    case DataType.Bool:
+                        this.AddData(dataEntry.Key, new GridData<bool>(true));
                         break;
-                    case Object:
-                        this.dataObject.Add(dataEntry.Key, null);
+                    case DataType.Vector2:
+                        this.AddData(dataEntry.Key, new GridData<Vector2>(new Vector2(6, 9)));
                         break;
-                    case Object:
-                        this.dataObject.Add(dataEntry.Key, null);
+                    case DataType.Object:
+                        this.AddData(dataEntry.Key, new GridData<object>(null));
                         break;
                 }
             }
         }
 
-        public void AddData(string name, object data)
+        public bool Contains(Vector2 queryPos)
+        {
+            if (!bounds.Contains(queryPos))
+            {
+                return false;
+            }
+
+            return (queryPos.x >= center.x - level.CellSize.x / 2 && queryPos.x <= center.x + level.CellSize.x / 2 &&
+                    queryPos.y >= center.y - level.CellSize.y / 2 && queryPos.y <= center.y + level.CellSize.y / 2);
+        }
+
+        public GridCell GetChild(Vector2 queryPos)
+        {
+            foreach (GridCell cell in childCells)
+            {
+                if (cell.Contains(queryPos))
+                {
+                    return cell;
+                }
+            }
+
+            return null;
+        }
+
+        public List<GridCell> GetLowestChild(Vector2 queryPos)
+        {
+            List<GridCell> path = new List<GridCell>();
+            GridCell childContainer = GetChild(queryPos);
+
+            if (childContainer != null)
+            {
+                path.Add(childContainer);
+                path.AddRange(childContainer.GetLowestChild(queryPos));
+            }
+            else if (this.Contains(queryPos))
+            {
+                path.Add(this);
+            }
+
+            return path;
+        }
+
+        public void AddData(string name, AbstractGridData data)
         {
             this.data.Add(name, data);
+        }
+
+        public Dictionary<string, AbstractGridData> GetData()
+        {
+            return this.data;
         }
 
         public object GetData(string name)
@@ -243,27 +421,34 @@ namespace DataStructure
         }
     }
 
-    public abstract class AbstractGridData {
-        public string name;
-
-        public AbstractGridData(string name) {
-            this.name = name;
+    public abstract class AbstractGridData
+    {
+        public AbstractGridData()
+        {
         }
     }
 
-    public class GridData<T> : AbstractGridData {
+    public class GridData<T> : AbstractGridData
+    {
         public T data;
 
-        public GridData(string name, T data) : base(name) {
+        public GridData(T data) : base()
+        {
             this.data = data;
         }
+
+        public override string ToString()
+        {
+            return data.ToString();
+        }
     }
-    
-    public enum DataType {
+
+    public enum DataType
+    {
         Float,
         Int,
         Bool,
-        Vec2,
+        Vector2,
         Object
     }
 }
--- Assets/DataStructure/Scripts/GridLevel.cs
+++ Assets/DataStructure/Scripts/GridLevel.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace DataStructure {
+namespace SimDataStructure
+{
     [CreateAssetMenu(menuName = "Data Structure/GridLevel")]
     public class GridLevel : ScriptableObject
     {
         public int Level;
         public Vector2 CellSize;
-        
-        public Dictionary<string, DataStructure.DataType> data = new Dictionary<string, DataStructure.DataType>();
+
+        public GenericDictionary<string, SimDataStructure.DataType> data = new GenericDictionary<string, SimDataStructure.DataType>();
     }
 }
